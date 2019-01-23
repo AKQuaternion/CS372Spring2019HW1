2263d764379a2fbc8e4e20a994fc5ef06ee8a2ee
Dylan Palmieri <dgpalmieri@gmail.com>
2019-01-23 13:21:53 -0900

    Add Dylan grade

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
    cout << "Professor Hartman doesn't need a grade." << endl;
    cout << "Nami - um.... Hi, Can I get an A... thanks" << endl;
    cout << "Bryant gets an A." << endl;
+   cout << "Dylan rolls a nat 20 on deception and receives an A." << endl;
    return 0;
 }
