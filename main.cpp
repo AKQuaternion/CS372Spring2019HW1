a352b0b8d6958fb67021f39bc922b8f2308aa7b1
Bryant <bgklug@alaska.edu>
2019-01-24 08:54:03 +1300

    Add grade for Bryant

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ using std::endl;
 int main() {
    cout << "Professor Hartman doesn't need a grade." << endl;
    cout << "Nami - um.... Hi, Can I get an A... thanks" << endl;
+   cout << "Bryant gets an A." << endl;
    return 0;
 }
