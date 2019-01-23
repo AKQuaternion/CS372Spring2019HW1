7155445a0dca3a1cb94c7c4d2abfc996d7b27779
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-01-23 14:02:37 -0900

    Add ASCII message
    Add Charles Emerson's desired grade message

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
    cout << "Nami - um.... Hi, Can I get an A... thanks" << endl;
    cout << "Bryant gets an A." << endl;
    cout << "Dylan rolls a nat 20 on deception and receives an A." << endl;
+   cout << "Charles - \x49\x27\x64\x20\x6c\x69\x6b\x65\x20\x61\x6e\x20\x41\x20\x70\x6c\x65\x61\x73\x65" << endl;
    return 0;
 }
