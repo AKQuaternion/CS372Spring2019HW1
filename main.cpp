b358927f22d9e4d7f8686c4878ba216c91ccabe8
Mark Underwood <mtunderwood@alaska.edu>
2019-01-23 20:04:58 -0900

    Add tedious but clever message

--- main.cpp
+++ main.cpp
@@ -10,5 +10,13 @@ int main() {
    cout << "Dylan rolls a nat 20 on deception and receives an A." << endl;
    cout << "Charles - \x49\x27\x64\x20\x6c\x69\x6b\x65\x20\x61\x6e\x20\x41\x20\x70\x6c\x65\x61\x73\x65" << endl;
    cout << "Bradley deserves an A." << endl;
+   cout << "if (Mark_Underwood.didGoodJob())" << endl
+        << "{" << endl
+        << "     Mark_Underwood.getFullPoints();" << endl
+        << "}" << endl
+        << "else" << endl
+        << "{" << endl
+        << "     Mark_Underwood.getWhatHeDeserves(deservedGrade);" << endl
+        << "}" << endl << endl;
    return 0;
 }
