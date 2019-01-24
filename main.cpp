081fca14db1686735987173c882d00df62771b32
bradleymorton <bradley.m.morton@gmail.com>
2019-01-23 18:10:46 -0900

    I added my name to the list to be printed.

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main() {
    cout << "Bryant gets an A." << endl;
    cout << "Dylan rolls a nat 20 on deception and receives an A." << endl;
    cout << "Charles - \x49\x27\x64\x20\x6c\x69\x6b\x65\x20\x61\x6e\x20\x41\x20\x70\x6c\x65\x61\x73\x65" << endl;
+   cout << "Bradley deserves an A." << endl;
    return 0;
 }
