9fa6589f463126aea833b3c19dd969b8d9b82f28
Laura Lundell <llundell@ymail.com>
2019-01-26 20:33:52 -0900

    Add Laura's HW1 Part 2

--- main.cpp
+++ main.cpp
@@ -21,5 +21,6 @@ int main() {
    cout << "Noah 01000111 01001001 01010110 01000101 00100000 01000001 00101011 00100000 01010000 01001100 01011010" << endl;
    cout << "Simeon deserves an A!" << endl;
    cout << "George Meier gets an A+ ... and some extra credit!" << endl;
+   cout << "I'll take an A please. Thanks, Laura" << endl;
    return 0;
 }
